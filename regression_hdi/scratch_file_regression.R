739f7c8d044922fd6fa6216154da0be46261de23
Immanuel Williams <immanuelwilliams@gato365.com>
2022-02-25 10:49:13 -0800

    create a scratch file

--- regression_hdi/scratch_file_regression.R
+++ regression_hdi/scratch_file_regression.R
@@ -6,6 +6,25 @@ library(MASS)
 suppressPackageStartupMessages(library(tidyverse))
 library(ggfortify)
 library(gridExtra)
+library(readxl)
+library(broom)
 
 
 setwd("G:/My Drive/03_research_and_development/01_research_topics/paper_0_CRD_main/lesson_plans/crd_paper_lessons/regression_hdi")
+hdi_df = read_xlsx('hdi_2015.xlsx') %>% 
+  select(-contains('HDI'),-Country)
+
+explanatory_variable <- 'Trade (% GDP)'
+response_variable <- 'Fertility Rate'
+
+f <- as.formula(
+  paste(response_variable, 
+        paste(explanatory_variable, collapse = " + "), 
+        sep = " ~ "))
+
+
+
+tidy(lm(y~x,data = hdi_df))
+
+
+

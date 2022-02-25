4c1fdf2a18536b7cb677ea2ce9268253a0cb017c
Immanuel Williams <immanuelwilliams@gato365.com>
2022-02-25 10:36:55 -0800

    Setting up R Shiny and proper files

---
+++ regression_hdi/scratch_file_regression.R
@@ -0,0 +1,11 @@
+
+
+library(shiny)
+library(bslib)
+library(MASS)
+suppressPackageStartupMessages(library(tidyverse))
+library(ggfortify)
+library(gridExtra)
+
+
+setwd("G:/My Drive/03_research_and_development/01_research_topics/paper_0_CRD_main/lesson_plans/crd_paper_lessons/regression_hdi")
